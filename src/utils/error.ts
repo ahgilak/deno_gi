3d3dcd3e10a92a0b804307098334b2536b7f1981
Angelo Verlain <geoangercola@gmail.com>
2023-12-11 17:11:05 +0200

    use a global GLib.Error
    to allow code like (error instanceof GLib.Error) to work

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,25 +1,28 @@
 import { cast_u64_ptr } from "../base_utils/convert.ts";
-import g from "../bindings/mod.js";
-import { createStruct } from "../types/struct.js";
+import { require } from "../gi.js";
 import { ExtendedDataView } from "./dataview.js";
 
 export function createGError(errorBuffer: ArrayBuffer) {
+  const GError = getGLibError();
+
   const dataView = new ExtendedDataView(errorBuffer);
   const pointer = cast_u64_ptr(dataView.getBigUint64());
 
-  return new ErrorStruct(pointer);
+  const error = new GError(pointer);
+
+  return error;
 }
 
-function createErrorStruct() {
-  const errorInfo = g.irepository.find_by_name(null, "GLib", "Error");
-  const errorGtype = g.registered_type_info.get_g_type(errorInfo);
-
-  const ErrorStruct = createStruct(errorInfo, errorGtype);
-
-  return ErrorStruct;
+export function getGLibError() {
+  return require("GLib", "2.0").Error;
 }
 
-// make sure GLib is loaded
-g.irepository.require(null, "GLib", "2.0", 0, null);
+// function createErrorStruct(pointer: Deno.PointerValue) {
 
-const ErrorStruct = createErrorStruct();
+//   const ErrorStruct = createStruct(errorInfo, errorGtype);
+
+//   return new ErrorStruct(pointer);
+// }
+
+// // make sure GLib is loaded
+// g.irepository.require(null, "GLib", "2.0", 0, null);
