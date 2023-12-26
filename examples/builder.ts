4fe4ba186dc50a9b62c2f98b960e911eb53f48de
Angelo Verlain <geoangercola@gmail.com>
2023-12-27 00:40:01 +0200

    restore examples/builder.ts import

--- examples/builder.ts
+++ examples/builder.ts
@@ -1,5 +1,4 @@
-import { require } from "../mod.ts";
-const Gtk = require("Gtk", "4.0");
+import Gtk from "https://gir.deno.dev/Gtk-4.0";
 
 const app = Gtk.Application.new("com.deno_gi.builder", 0);
 
