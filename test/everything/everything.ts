0383f553c560d6f90a81e22e38c95765d5a820ea
Angelo Verlain <geoangercola@gmail.com>
2024-05-26 23:12:27 +0200

    chore: add sample test

--- test/everything/everything.ts
+++ test/everything/everything.ts
@@ -1,3 +1,36 @@
-import Everything from "https://gir.deno.dev/Everything-1.0";
+// import Everything from "https://gir.deno.dev/Everything-1.0";
 
-console.log("Everything", Everything);
+import { require } from "../../mod.ts";
+import { assertEquals } from "../test_deps.ts";
+
+const Everything = require("Everything", "1.0");
+
+interface GIType {
+  name: string;
+  type:
+    | "bigint"
+    | "boolean"
+    | "function"
+    | "number"
+    | "object"
+    | "string"
+    | "symbol"
+    | "undefined";
+  value: unknown;
+}
+
+const types: GIType[] = [
+  {
+    name: "filename",
+    type: "string",
+    value: "hello",
+  },
+];
+
+for (const { name, type, value } of types) {
+  Deno.test(`const_return_${name}`, () => {
+    const value = Everything[`const_return_${name}`]();
+
+    assertEquals(typeof value, type, `has incorrect return type`);
+  });
+}
